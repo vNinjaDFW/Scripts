4df18fac3758f450d07484b8379db7850e1d09fa
vNinjaDFW <ryanpatel@gmail.com>
2020-03-06 11:39:02 -0600

    Create Get_VMs_Windows.ps1

---
+++ VMware/Get_VMs_Windows.ps1
@@ -0,0 +1,38 @@
+# Select a vCenter
+Get-Content vCenterList.txt
+
+[int]$ivCenter = Read-Host "`nSelect a vCenter Number:"
+
+$vCenter = (Get-Content vCenterList.txt -TotalCount ($ivCenter+1))[-1]
+$vCenter = $vCenter.substring(4)
+$vCenter
+
+If ($vCenter -eq 'End of List****') {
+    Write-Host "Invalid Selection. Exiting."
+    exit
+}
+
+Write-Host "`nYou Picked: "$vCenter `n 
+
+Start-sleep -s 3
+ 
+# Connect to selected vCenter
+Connect-VIServer $vCenter -WarningAction SilentlyContinue
+
+# Select the Cluster
+Write-Host ""
+Write-Host "Choose the Cluster:"
+Write-Host ""
+$iCluster = Get-Cluster | Select Name | Sort-object Name
+$i = 1
+$iCluster | %{Write-Host $i":" $_.Name; $i++}
+$dCluster = Read-Host "Enter the number for the Cluster:"
+$sCluster = $iCluster[$dCluster -1].Name
+Write-Host "You picked:" $sCluster"."
+
+$VMs = Get-Cluster $sCluster | Get-VM
+Foreach ($vm in $VMs){
+     if ((Get-View $vm).Guest.GuestFullName -match "Windows"){
+               Write-Host $vm.Name, " ---->" , (Get-View $vm).Guest.GuestFullName
+     }
+}
